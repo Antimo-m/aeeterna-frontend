e7726827c2a734090c081734834f06c3888768f3
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-02-05 17:17:29 +0100

    feat; page cart e logic cart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,58 +3,69 @@ import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
-function CartContextProvider({children}){
+function CartContextProvider({ children }) {
     const [cartList, setCartList] = useState([]);
 
-    function addOrdine(newProduct){            
-        if(cartList.find((product) => product.slug === newProduct.slug) === undefined){
+    function addCart(newProduct) {
+        
+        if (cartList.find((product) => product.slug === newProduct.slug) === undefined) {
             let newOrder = {
                 ...newProduct,
                 quantity: 1
             };
+            console.log(cartList);
+            console.log(newOrder);
             
+            console.log([...cartList, newOrder]);
+
             setCartList([...cartList, newOrder]);
-        }else{
+        } else {
             const copyArray = cartList.map((product) => {
-                if(product.slug === newProduct.slug){
+                if (product.slug === newProduct.slug) {
                     const newQuantity = product.quantity + 1;
                     return {
                         ...product,
                         quantity: newQuantity,
                     }
-                }else{
+                } else {
                     return product;
                 }
-            })            
+            })
+            console.log(copyArray);
+
             setCartList(copyArray)
         }
     }
 
-    function removeOrdine(newProduct, indexDelete){
+    function removeProduct(indexDelete) {
         const copyArray = [...cartList];
-        if(copyArray[indexDelete].quantity === 1){
+        if (copyArray[indexDelete].quantity === 1) {
             const newArray = cartList.toSpliced(indexDelete, 1)
             setCartList(newArray);
-        }else{
+        } else {
             copyArray[indexDelete].quantity = copyArray[indexDelete].quantity - 1;
             setCartList(copyArray)
         }
     };
 
-    function resetCarrello(){
+    function resetCarrello() {
         setCartList([]);
     }
-    
-    let total = 0;
-    cartList.forEach(({ quantity, price }) => {
-        total += (quantity * price);
-    })
+
+    function calcTotal(array) {
+        let total = 0;
+        array.forEach(({ quantity, price }) => {
+            total += (quantity * price);
+        })
+        return total
+    }
+
 
     const cartValue = {
-        cartList, 
-        addOrdine,
-        removeOrdine,
-        total,
+        cartList,
+        addCart,
+        removeProduct,
+        calcTotal,
         resetCarrello,
     }
 
@@ -65,9 +76,9 @@ function CartContextProvider({children}){
     )
 }
 
-function useCart(){
+function useCart() {
     const value = useContext(CartContext);
     return value
 }
 
-export {useCart, CartContextProvider}
+export { useCart, CartContextProvider }
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,83 @@
-export default function Home(){
+import { Link } from "react-router-dom";
+import { useCart } from "../contexts/CartContext";
+import style from "../styles/CartPage.module.css"
+
+export default function Home() {
+    const { cartList, removeProduct, calcTotal } = useCart();
+    console.log(cartList);
+
     return (
         <>
-        
+            <main className={style.main}>
+                {cartList.length === 0 ?
+                    <div className={style.emptyCart}>
+                        <h1>IL TUO CARRELLO È VUOTO</h1>
+                        <Link to={"/search"} className={style.checkOut}>VAI ALLO SHOPPING</Link>
+                    </div>
+                    :
+                    <section className={style.sectionCart}>
+                        <div className={style.sectionProduct}>
+                            {cartList.map((product, index) => (
+                                <div className={style.card}>
+                                    <img src={product.image} alt="" />
+                                    <div className={style.bodyCard}>
+                                        <div className={style.description}>
+                                            <h2>{product.name}</h2>
+                                            <span>
+                                                <h3>Quantità: {product.quantity}</h3>
+                                                <h3>Price: {product.price}</h3>
+                                            </span>
+                                        </div>
+                                        <a onClick={() => removeProduct(index)}>RIMUOVI</a>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className={style.sectionSummary}>
+                            <h2>TOTALE ORDINE</h2>
+                            <div className={style.tableSummary}>
+                                <div className={style.card}>
+                                    <h3>SubTotal</h3>
+                                    <h3>{calcTotal(cartList).toFixed(2)}€</h3>
+                                </div>
+                                <div className={style.card}>
+                                    {calcTotal(cartList) < 45 ?
+                                        <>
+                                            <h3>Costi Di Spedizione</h3>
+                                            <h3>4.99€</h3>
+                                        </>
+
+                                        :
+                                        <>
+                                            <h3>Costi Di Spedizione</h3>
+                                            <div className={style.shippingContainer}>
+                                                <h3 className={style.oldPrice}>4.99€</h3>
+                                                <h3>0.00€</h3>
+                                            </div>
+                                        </>
+                                    }
+                                </div>
+                                <div className={style.card}>
+                                    {calcTotal(cartList) < 45 ?
+                                        <>
+                                            <h3>Totale</h3>
+                                            <h3>{calcTotal(cartList) + 4.99}</h3>
+                                        </>
+
+                                        :
+                                        <>
+                                            <h3>Totale</h3>
+                                            <h3>{calcTotal(cartList).toFixed(2)}€</h3>
+                                        </>
+                                    }
+                                </div>
+                            </div>
+                            <button className={style.checkOut}>CHECKOUT</button>
+                        </div>
+                    </section>
+                }
+
+            </main>
         </>
     )
 }
