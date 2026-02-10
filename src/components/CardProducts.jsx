a14fabc23847b180bc979efe43e5221c4c7f1cf9
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-02-10 15:04:12 +0100

    feat modifica quantità in tutte le product card e cambiamento di offset in page

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -2,18 +2,18 @@ import MainStyle from "../styles/Main.module.css";
 import { useWishList } from "../contexts/WishListContext";
 import { useCart } from "../contexts/CartContext"
 import { Link } from "react-router-dom";
-import {useMessage} from "../contexts/MessageContext"
+import { useMessage } from "../contexts/MessageContext"
 
 export default function CardProduct({ product }) {
 
-    const { addCart } = useCart();
+    const { addCart, inCart, updateQuantityDetails, returnQuantity } = useCart();
     const { removeWishList, addWishList, inWishList } = useWishList();
 
-    const {showMessage} = useMessage()
+    const { showMessage } = useMessage()
 
     const HandleAddToCart = () => {
         addCart(product)
-        showMessage(`${product.name} è ora nel tuo carello` )
+        showMessage(`${product.name} è ora nel tuo carello`)
     }
     return (
         <>
@@ -28,7 +28,15 @@ export default function CardProduct({ product }) {
                 </span>
 
                 <div className={MainStyle.buttonGroup}>
-                    <button onClick={HandleAddToCart} className={`addCartHover ${MainStyle.button}`}>AGGIUNGI AL CARRELLO</button>
+                    {inCart(product.slug) ?
+                        <div className={MainStyle.buttonQuantity}>
+                            <button className="addCartHover" onClick={() => updateQuantityDetails(product.slug, returnQuantity(product.slug) - 1)}>-</button>
+                            <span>{returnQuantity(product.slug)}</span>
+                            <button className="addCartHover" onClick={() => updateQuantityDetails(product.slug, returnQuantity(product.slug) + 1)}>+</button>
+                        </div>
+                        :
+                        <button className={`addCartHover ${MainStyle.buttonCart}`} onClick={() => addCart(product)}>Aggiungi al carrello</button>
+                    }
                     <button className={inWishList(product) ? "btninWish" : "btnWish"} onClick={() => { inWishList(product) ? removeWishList(product) : addWishList(product) }}>
                         <i className="bi bi-heart"></i>
                     </button>
