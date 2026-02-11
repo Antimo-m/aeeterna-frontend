258849ead88a6a0f7f89d20ca0618a32afd8b1b2
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-02-11 13:51:16 +0100

    fix frecce cambio immagine

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -54,9 +54,9 @@ export default function ProductDetails() {
         return <NotFoundProduct />
     }
 
-    function updateImageIndex(newQuantity){
-        if(newQuantity < 0)return;
-        if(newQuantity > product.images.length - 1)return
+    function updateImageIndex(newQuantity) {
+        if (newQuantity < 0) return;
+        if (newQuantity > product.images.length - 1) return
         setIndexImage(newQuantity)
     }
 
@@ -76,9 +76,11 @@ export default function ProductDetails() {
                                 </span>
                             </div>
                             <div className={style.img}>
-                                <span onClick={() => updateImageIndex( indexImage - 1)} className={`${style.leftArrow} ${style.arrowHover}`}><i className="bi bi-arrow-left-short"></i></span>
-                                <img src={product.images[indexImage].path} alt="" />
-                                <span onClick={() => updateImageIndex( indexImage + 1)} className={`${style.rightArrow} ${style.arrowHover}`}><i className="bi bi-arrow-right-short"></i></span>
+                                <div className={style.figureGroup}>
+                                    <span onClick={() => updateImageIndex(indexImage - 1)} className={`${style.leftArrow} ${style.arrowHover}`}><i className="bi bi-arrow-left-short"></i></span>
+                                    <img src={product.images[indexImage].path} alt="" />
+                                    <span onClick={() => updateImageIndex(indexImage + 1)} className={`${style.rightArrow} ${style.arrowHover}`}><i className="bi bi-arrow-right-short"></i></span>
+                                </div>
                             </div>
                             <div className={style.description}>
                                 <div className={style.sectionPrice}>
